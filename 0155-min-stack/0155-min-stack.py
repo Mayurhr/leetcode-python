bbe82a16a0006e11e97ebee0dd63c9fa34f63f64
Mayur H R <162894895+Mayurhr@users.noreply.github.com>
2026-08-16 10:32:02 +0530

    Time: 149 ms (78.87%), Space: 24.8 MB (39.32%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,22 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack=[]
+        self.min_stack=[]        
+
+    def push(self, value):
+        self.stack.append(value)
+        if not self.min_stack or value<=self.min_stack[-1]:
+            self.min_stack.append(value)     
+
+    def pop(self):
+        if self.stack[-1]==self.min_stack[-1]:
+            self.min_stack.pop()
+        self.stack.pop()
+        
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.min_stack[-1]
+        
